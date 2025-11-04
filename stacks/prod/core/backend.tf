08f94e27ea4c26a99d61698bad00705f58d055ed
naka0519 <naka20000519@gmail.com>
2025-11-04 22:50:29 +0900

    fix backend

--- stacks/prod/core/backend.tf
+++ stacks/prod/core/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "nw-iac-tfstate-global-20251101" # TODO: change to your bucket
-    # dynamodb_table = "tfstate-locks-global-20251101"  # TODO: change to your table lock false
-    use_lockfile   = false
+    dynamodb_table = "tfstate-locks-global-20251101"  # TODO: change to your table lock false
+    # use_lockfile   = false
     region         = "ap-northeast-1"
     key            = "network/prod-core/terraform.tfstate"
     encrypt        = true
